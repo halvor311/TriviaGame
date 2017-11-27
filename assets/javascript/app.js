23c43120e4f17afbb3ecef7d2bad4ee9d0cbaf7f
Gabriel Vermund <gabrielvermund@Gabriels-MBP.campustech.net>
2017-11-27 16:55:53 -0500

    Still Lost

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -64,7 +64,7 @@ $(document).ready(function(){
         }
     }
     $(".questionContainer").append("<p id='countdown'></p>"); 
-    $(".questions").append("<h1 class = questions></h1>" + questionArray[0].question.answerOne);
+    $(".questions").append("<h1 class = questions></h1>" + questionOne.question.answerOne);
     $(".answers").append("<p class = answers></h1>" + questionArray[0]);
     console.log(questionArray);
     }); 
